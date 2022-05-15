87c1600b564bd1635fef9e3ef431590f6d7abad6
zhengminhui <zhmh1025@hotmail.com>
2022-05-15 17:25:52 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 hexo g
 cp .gitignore favicon.ico public/
-echo "cp .gitignore favicon.ico public/"
+echo "cp .gitignore images/favicon16.png images/favicon32.png public/"
 hexo d
 hexo clean
 rm -rf .deploy_git
