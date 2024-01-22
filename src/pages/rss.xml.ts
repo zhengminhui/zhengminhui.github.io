d27db304d90645cbb7fb375ae6a98ea510f6dc42
zhengminhui <zhmh1025@hotmail.com>
2024-01-22 20:42:40 +0800

    fix types error

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,18 @@
 import rss from "@astrojs/rss";
+import type { RSSOptions } from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import getSortedPosts from "@utils/getSortedPosts";
 import slugify from "@utils/slugify";
 import { SITE } from "@config";
 
-export async function GET(context) {
+export async function GET(context: RSSOptions) {
   const posts = await getCollection("blog");
+  console.log(context.site, SITE.website);
   const sortedPosts = getSortedPosts(posts);
   return rss({
     title: SITE.title,
     description: SITE.desc || "description",
-    site: SITE.website,
+    site: context.site,
     items: sortedPosts.map(p => {
       const { data } = p;
       return {
